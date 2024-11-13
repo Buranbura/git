203b2536d51f1ba1c8603081d64d5b284acca5f0
Buranbura <3344179229@qq.com>
2024-11-13 15:26:19 +0800

    1

---
+++ 2024.11.13/main.c
@@ -0,0 +1,40 @@
+
+//飞机
+/*#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("     **\n");
+    printf("************\n");
+    printf("************\n");
+    printf("    *  *\n");
+    printf("    *  *\n");
+    return 0;
+}*/
+
+
+
+//输入多组数，判断是否为天才
+/*#include <stdio.h>
+int main()
+{
+    int iq;
+    while(scanf("%d",&iq)==1)
+    {
+        if(iq>=140)
+            printf("Genius\n");
+    }
+    return 0;
+}*/
+
+
+
+
+
+
+
+
+
+
+
